6a75522f2b15fe35957754ffd95253ff3ef79f08
Medzy <Medhanshpatel18@gmail.com>
2025-05-10 23:05:57 +0530

    Updated supabase.ts

--- src/utils/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { Database } from './types';
-
-// Initialize Supabase client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
-
-export const supabase: SupabaseClient<Database> = createClient<Database>(supabaseUrl, supabaseAnonKey);
